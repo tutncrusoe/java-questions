68a891aa78be7cc33ffc238424727b404c6be243
huydinhanhle <huydinhanhle@gmail.com>
2022-10-31 00:08:37 +0700

    tricky java

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,27 @@ public class Main {
 
         NullAsArgument.foo(null);
 
+
         UseLforLong useLforLong = new UseLforLong();
         System.out.println(useLforLong.longWithL);
         System.out.println(useLforLong.longWithoutL);
 
+        /**
+         * Java is always pass-by-value.
+         * Unfortunately, when we pass the value of an object, we are passing the reference to it.
+         * There is no such thing as "pass-by-reference" in Java.
+         * This is confusing to beginners.
+         * The key to understanding this is that something like:
+         */
+        NullPointerExceptionImpl nullPointerException_;
+        // is not a NullPointerExceptionImpl; it's actually a pointer to a NullPointerExceptionImpl.
+
+        /**
+         * So when you have:
+         */
         NullPointerExceptionImpl nullPointerException = new NullPointerExceptionImpl();
         nullPointerException.printString(null, 3);
+        // you're essentially passing the address of the created UseLforLong object to the printString method.
 
 
     }
